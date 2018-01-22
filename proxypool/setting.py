7bc97d6145f364121bf339545efb8ee2b8d3af75
feiyupianshui <ed273030@gmail.com>
2018-01-22 17:57:45 +0800

    20180122

--- proxypool/setting.py
+++ proxypool/setting.py
@@ -1,6 +1,6 @@
 # Redis数据库的地址和端口
-HOST = 'localhost'
-PORT = 'secret'
+REDIS_HOST = 'localhost'
+REDIS_PORT = 'secret'
 DATABASE = 1
 
 # 如果Redis有密码，则添加这句密码，否则设置为None或''
@@ -9,15 +9,14 @@ PASSWORD = ''
 # 测试代理的网站
 TEST_URL = "https://www.baidu.com/"
 
-# 测试代理的超时时间
-TEST_TIME = 9
-
-# 数据库代理数量上下限，即数量在20-200之间的话，不会添加
-OVERFLOW = 100
-FLOOR = 20
-
-# 检查休眠时间
-CHECK_POOL_CYCLE = 20
-VALID_PROXY_CYCLE = 600
-
 REDIS_KEY = 'proxies'
+
+# 代理分数
+MAX_SCORE = 100
+MIN_SCORE = 0
+INITIAL_SCORE = 10
+
+VALID_STATUS_CODES = [200, 302]
+
+# 代理池数量界限
+POOL_UPPER_THRESHOLD = 50000
