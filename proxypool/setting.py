d0b3477fa97ce07601402434019997faf49e3c72
feiyupianshui <ed273030@gmail.com>
2018-01-23 17:44:03 +0800

    20180123

--- proxypool/setting.py
+++ proxypool/setting.py
@@ -20,3 +20,16 @@ VALID_STATUS_CODES = [200, 302]
 
 # 代理池数量界限
 POOL_UPPER_THRESHOLD = 50000
+
+# 检查周期
+TESTER_CYCLE = 20
+# 获取周期
+GETTER_CYCLE = 300
+
+# 测试API，建议抓哪个网站测哪个
+TEST_URL = 'http://www.baidu.com'
+
+# API配置
+API_HOST = '0.0.0.0'
+API_PORT = 5555
+
