a2ce11cbc56a28f298b2af8183c4ee91c33b69c3
feiyupianshui <ed273030@gmail.com>
2018-01-29 17:54:02 +0800

    20180129

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from proxypool.scheduler import Scheduler
 import sys
 import io
 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
 def main():
     try:
         s = Scheduler()
