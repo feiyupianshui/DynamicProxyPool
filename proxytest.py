5e29f9fb3f24a81334524649c54a460b11e8a14b
feiyupianshui <ed273030@gmail.com>
2018-01-31 17:46:44 +0800

    20180131

--- proxytest.py
+++ proxytest.py
@@ -9,3 +9,9 @@ proxies = {
     'http': 'http://' + proxy,
     'https': 'https://' + proxy,
 }
+
+print(TEST_URL)
+response = requests.get(TEST_URL, proxies=proxies, verify=False)
+if response.status_code == 200:
+    print('Successfully')
+    print(response.text)
