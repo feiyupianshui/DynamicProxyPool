79013af15e6d234d4ff96f4a1dc97b31c9ff9603
feiyupianshui <ed273030@gmail.com>
2018-02-05 18:00:06 +0800

    20180205

--- proxypool/db.py
+++ proxypool/db.py
@@ -91,6 +91,13 @@ class RedisClient(object):
         """
         return self.db.zrevrange(REDIS_KEY, start, stop - 1)
 
+    conn = RedisClient()
+
+    def set(proxy):
+        result = conn.add(proxy)
+        print(proxy)
+        print('录入成功' if result else '录入失败')
+
 if __name__ == '__main__':
     conn = RedisClient()
     result = conn.batch(680, 688)
